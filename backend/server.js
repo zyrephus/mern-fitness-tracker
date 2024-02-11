5bebd685c5c1a7edee1f24cc4c7b33d01f5d983c
Zyrephus <wilsonchen315@gmail.com>
2024-02-10 21:27:14 -0500

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
 
 // Express app
@@ -17,7 +18,14 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/workouts', workoutRoutes)
 
-// Listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('Listening on port.')
-})
+// Connect to database
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        // Listen for requests
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to databvase and listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
