994469c3ca0a98f96293e1ec2279450870973f66
Zyrephus <wilsonchen315@gmail.com>
2024-02-14 20:22:01 -0500

    Signing tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, { expiresIn: '3d' })
+}
 
 // Login a user
 const loginUser = async (req, res) => {
@@ -12,7 +17,10 @@ const signupUser = async (req, res) => {
   try {
     const user = await User.signup(email, username, password)
 
-    res.status(200).json({email, username, user})
+    // Create a token
+    const token = createToken(user._id)
+
+    res.status(200).json({email, username, token})
   }
   catch (error) { 
     res.status(400).json({error: error.message})
